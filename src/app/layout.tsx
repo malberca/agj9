48b51626dad0ad683dd60ef573d67e30a792dee7
malberca <malberca@gmail.com>
2026-05-15 13:33:04 -0300

    Add Metricool tracker script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/resources/custom.css";
 
 import classNames from "classnames";
 import { Anton } from "next/font/google";
+import Script from "next/script";
 
 import {
   Background,
@@ -210,6 +211,16 @@ export default async function RootLayout({
               </Flex>
             </Flex>
             <Footer />
+            <Script
+              id="metricool-tracker"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}
+                  loadScript(function(){beTracker.t({hash:"adfd061adcb9ed161f6c275e1828245d"})});
+                `,
+              }}
+            />
             <LitoChat />
           </div>
         </Column>
